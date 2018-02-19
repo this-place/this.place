75c51c027b0fa3ed934461af02e9d0428de7b6d0
nicholaschuayunzhi <nicholaschuayunzhi@gmail.com>
2018-02-19 23:26:14 +0800

    feat: add block feature
    Add basic block behaviour - displacement and collision
    Contains bug where you can merge blocks together

---
+++ Assets/Scripts/BlockBehaviour.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockBehaviour : MonoBehaviour
+{
+    public float Speed = 1f;
+
+    private Vector3 _originalPosition;
+    private Vector3 _targetPosition;
+    private bool _isDisplaced = false;
+    private bool _isTranslating = false;
+
+    // Use this for initialization
+    private void Start()
+    {
+        _originalPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (_isTranslating)
+        {
+           TranslateBlock();
+        }
+    }
+
+    public void MoveBlock(Vector3 dir, float faceLength)
+    {
+        if (_isTranslating)
+        {
+            return;
+        }
+
+        float halfFaceLength = faceLength / 2;
+
+        if (_isDisplaced)
+        {
+            Vector3 dirToOriginal = _originalPosition - transform.position;
+
+            //Fire Raycast to see if block can move to position
+            bool hit = Physics.Raycast(transform.position, dirToOriginal.normalized, halfFaceLength * 2.75f); //some terrible hack
+            if (!hit)
+            {
+                _targetPosition = _originalPosition;
+                _isTranslating = true;
+            }
+        }
+        else
+        {
+            bool hit = Physics.Raycast(_originalPosition, dir, halfFaceLength * 2.75f); //some terrible hack
+            if (!hit)
+            {
+                _targetPosition = _originalPosition + (dir * faceLength);
+                _isTranslating = true;
+            }
+        }
+    }
+
+    private void TranslateBlock()
+    {
+        Vector3 translateDir = _targetPosition - transform.position;
+        transform.Translate(translateDir.normalized * Time.deltaTime * Speed);
+        if (Vector3.Distance(transform.position, _targetPosition) <= 0.01f)
+        {
+            transform.position = _targetPosition;
+            _isTranslating = false;
+
+            _isDisplaced = _targetPosition != _originalPosition;
+        }
+    }
+
+    
+}
---
+++ Assets/Scripts/BlockFaceBehaviour.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using NUnit.Framework;
+using UnityEngine;
+
+public class BlockFaceBehaviour : MonoBehaviour
+{
+
+    public BlockBehaviour Block;
+
+    private Color _originalColor;
+    private Renderer _renderer;
+
+    private Vector3 _normal;
+    private float _faceLength;
+
+    private void Start()
+    {
+        _renderer = GetComponent<Renderer>();
+        _originalColor = _renderer.material.color;
+
+        MeshFilter meshFilter = GetComponent<MeshFilter>();
+        Mesh mesh = meshFilter.mesh;
+
+        Vector3[] localVertices = mesh.vertices;
+        Assert.AreEqual(4, localVertices.Length);
+
+        // convert local vertex into world space coordinate
+        Vector3[] worldCoord = new Vector3[localVertices.Length];
+        for (int i = 0; i < localVertices.Length; i++)
+        {
+            worldCoord[i] = transform.TransformPoint(localVertices[i]);
+
+        }
+
+        // Choice of which coordinates to use was done via trial and error...
+        _faceLength = Vector3.Distance(worldCoord[0], worldCoord[2]);
+        _normal = CalculateFaceNormal(worldCoord);
+    }
+
+    private static Vector3 CalculateFaceNormal(Vector3[] worldCoords)
+    {
+        // Unity uses clockwise winding order for face normals
+        // https://forum.unity.com/threads/unity-has-a-clockwise-winding-order.129923/
+        Vector3 sideA = worldCoords[1] - worldCoords[0];
+        Vector3 sideB = worldCoords[2] - worldCoords[0];
+
+        return Vector3.Cross(sideA, sideB);
+    }
+
+    private void OnMouseEnter()
+    {
+        // TODO: Expose color to inspector
+        _renderer.material.color = Color.black;
+    }
+
+    private void OnMouseExit()
+    {
+        _renderer.material.color = _originalColor;  
+    }
+
+    private void OnMouseDown()
+    {
+
+        Block.MoveBlock(_normal, _faceLength);
+    }
+}
