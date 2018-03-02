4a14ee2bb9c78ce9ba1f11be5132bfded19c7b4f
A0162011A <e0112235@u.nus.edu>
2018-03-02 13:23:24 +0800

    feat: mouse can be used to rotate camera

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -14,35 +14,62 @@ public class CameraController : MonoBehaviour
     //used for rotation
     private float _originalY;
     private float _targetY;
-    private bool _isRotating;
     private float _speed = 380f;
+    private bool _isKeyboardRotating;
+    private bool _isMouseRotating;
     private float _movedAmount;
+    private float _mouseX;
+    private float _mouseY;
+    private float _mouseXSpeed = 1;
+    private float _mouseYSpeed = 0.1f;
+    private float _currentYDisplacement;
+    private const float MaxYDisplacement = 30;
+
 
     private void Start()
     {
         _offset = transform.position - PlayerObject.transform.position;
-        _offset.y = 0;
+        //_offset.y = 0;
         _yValue = transform.position.y;
         _originalY = transform.position.y;
     }
 
     private void Update()
     {
-        if ((Input.GetAxis("CameraControl") == 1 || Input.GetAxis("CameraControl") == -1) && !_isRotating)
+        Vector3 newPosition = PlayerObject.transform.position + _offset;
+        //newPosition.y = _yValue;
+        transform.position = newPosition;
+
+        if (Input.GetAxis("CameraControl") != 0 && !_isMouseRotating)
         {
-            _originalY = transform.eulerAngles.y;
-            _targetY = transform.eulerAngles.y + Input.GetAxis("CameraControl") * 90;
-            _isRotating = true;
-            _movedAmount = 0;
+            _isKeyboardRotating = true;
         }
 
-        if (_isRotating)
+        if (Input.GetAxis("CameraControl") == 0 && !_isMouseRotating)
         {
-            RotateCamera();
+            _isKeyboardRotating = false;
+        }
+
+        if (Input.GetMouseButtonDown(1) && !_isKeyboardRotating)
+        {
+            _isMouseRotating = true;
+            _mouseX = Input.mousePosition.x;
+            _mouseY = Input.mousePosition.y;
+        }
+
+        if (Input.GetMouseButtonUp(1) && !_isKeyboardRotating)
+            _isMouseRotating = false;
+
+        if (_isKeyboardRotating)
+            RotateKeyboardCamera();
+
+        if (_isMouseRotating)
+        {
+            RotateMouseCamera();
         }
     }
 
-    private void RotateCamera()
+    private void RotateKeyboardCamera()
     {
         float rotateDir = (_targetY - _originalY)/90;
         float moveAmount = rotateDir * Time.deltaTime * _speed;
@@ -53,19 +80,42 @@ public class CameraController : MonoBehaviour
             moveAmount -= _movedAmount + 90;
         transform.RotateAround(PlayerObject.transform.position, Vector3.up, moveAmount);
         if (Mathf.Abs(_movedAmount) >= 90f)
+    }
+
+    private void RotateMouseCamera()
+    {
+        float rotateXAmount = (_mouseX - Input.mousePosition.x) * _mouseXSpeed;
+        float rotateYAmount = (_mouseY - Input.mousePosition.y) * _mouseYSpeed;
+        _currentYDisplacement += rotateYAmount;
+        if (_currentYDisplacement > MaxYDisplacement)
         {
-            _isRotating = false;
+            rotateYAmount -= _currentYDisplacement - MaxYDisplacement;
+            _currentYDisplacement = MaxYDisplacement;
         }
+        else if (_currentYDisplacement < -MaxYDisplacement)
+        {
+            rotateYAmount -= _currentYDisplacement + MaxYDisplacement;
+            _currentYDisplacement = -MaxYDisplacement;
+        }
+        transform.RotateAround(PlayerObject.transform.position, Vector3.up, rotateXAmount);
+        Vector3 normalVector = GetNormalVector();
+        transform.RotateAround(PlayerObject.transform.position, normalVector, rotateYAmount);
+        _mouseX = Input.mousePosition.x;
+        _mouseY = Input.mousePosition.y;
 
         PlayerObject.GetComponent<PlayerController>().UpdateCamera();
         _offset = transform.position - PlayerObject.transform.position;
-        _offset.y = 0;
+        //_offset.y = 0;
+    }
+
+    private Vector3 GetNormalVector()
+    {
+        Vector3 playerFacing = transform.position - PlayerObject.transform.position;
+        Vector3 planeCompletion = playerFacing - Vector3.up;
+        return Vector3.Cross(playerFacing, planeCompletion);
     }
 
     private void LateUpdate()
     {
-        Vector3 newPosition = PlayerObject.transform.position + _offset;
-        newPosition.y = _yValue;
-        transform.position = newPosition;
     }
 }
