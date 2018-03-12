3bccb88e129c99a69dad2955f414f99dac94f6d4
Stanley Tay <saintpatricktay@gmail.com>
2018-03-12 22:41:24 +0800

    feat: fragile particle system

--- Assets/ScriptableObjects/Plugin Scripts/FragilePlugin.cs
+++ Assets/ScriptableObjects/Plugin Scripts/FragilePlugin.cs
@@ -6,12 +6,26 @@ using UnityEngine;
 public class FragilePlugin : BlockPlugin
 {
     public float BreakAfterTime;
+    public ParticleSystem FragileEmitter;
+
+    private bool _isDestroyingItself;
+    private ParticleSystem _fragileEmitterInstantiated;
 
     public override void OnUpdate()
     {
-        if (_block.IsTranslating() || _block.IsPlayerStandingOn())
+        if (!_isDestroyingItself && (_block.IsTranslating() || _block.IsPlayerStandingOn()))
         {
+            _fragileEmitterInstantiated = Instantiate(FragileEmitter, _block.gameObject.transform.position, _block.gameObject.transform.rotation) as ParticleSystem;
+            _fragileEmitterInstantiated.GetComponent<ParticleSystemRenderer>().material = _block.gameObject.GetComponent<Renderer>().material;
+            _fragileEmitterInstantiated.Play();
+            Destroy(_fragileEmitterInstantiated.gameObject, BreakAfterTime + 1.5f);
             Destroy(_block.gameObject, BreakAfterTime);
+            _isDestroyingItself = true;
+        }
+        if (_isDestroyingItself && _block.IsTranslating())  // To make the emitter follow the block
+        {
+            _fragileEmitterInstantiated.gameObject.transform.position = _block.gameObject.transform.position;
+            _fragileEmitterInstantiated.gameObject.transform.rotation = _block.gameObject.transform.rotation;
         }
     }
 }
