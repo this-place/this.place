800695485ed3b79dcbc0e731d3fde3914c009d19
jo-lyn <30170203+jo-lyn@users.noreply.github.com>
2018-03-22 19:31:55 +0800

    feat: 6 faces uv map (#89)
    * chore: add six faces atlas test texture
    
    * feat: add 6 faces uv mapping
    
    * fix: proper mapping for each faces
    
    * chore: update mapping for face highlight
    
    * chore: update sand atlas
    
    * fix: remove smoothness
    
    * feat: add grass-dirt texture
    
    * fix: adjust highlight color
    
    * fix: adjust highlight color for sand block
    
    * feat: add dirt texture
    
    * chore: update grass texture
    
    * fix: remove specular and reflection from sand material
    
    * refactor: rename grass texture
    
    * chore: update grass texture
    
    * chore: add dirt texture atlas
    
    * feat: add grass top prefab
    
    * chore: add placeholder texture for grass top prefab
    
    * chore: tidy folder
    
    * chore: tidy scene
    
    * test: blender cube for grass top block
    
    * test: blender cube
    
    * fix: incorrect vertex mapping
    
    * chore: remove test file
    
    * chore: code cleanup
    
    * chore: create prototype blender block
    
    * chore: replace grass top placeholder texture
    
    * chore: update grass top block prefab
    
    * chore: create full dirt block prefab
    
    * chore: update full dirt block prefab
    
    * refactor: folder restructure and renaming
    
    * chore: move BlenderCube to Prototypes folder
    
    * chore: update lighting scene

--- Assets/Scripts/UVMap.cs
+++ Assets/Scripts/UVMap.cs
@@ -8,9 +8,6 @@ public class UVMap : MonoBehaviour, ITransparentRenderer
 
     public Material BlockMaterial;
     public Material TransparentMaterial;
-    private float x = 0;
-    private float y = 1;
-    private const float PixelSize = 2;
     private Mesh _mesh;
     private Renderer _renderer;
 
@@ -23,53 +20,50 @@ public class UVMap : MonoBehaviour, ITransparentRenderer
     }
 
 
-    public void SetHighlightTexture()
-    {
-        x = 1;
-        SetUVs();
-    }
-
     public void SetNormalTexture()
     {
-        x = 0;
         SetUVs();
     }
 
     private void SetUVs()
     {
-        float tilePerc = 1 / PixelSize;
+        Vector2[] blockUVs = (Vector2[])_mesh.uv.Clone();
 
-        float umin = tilePerc * x;
-        float umax = tilePerc * (x + 1);
-        float vmin = tilePerc * y;
-        float vmax = tilePerc * (y + 1);
+        // Top
+        blockUVs[4] = new Vector2(0.0f, 0.75f);
+        blockUVs[5] = new Vector2(0.25f, 0.75f);
+        blockUVs[8] = new Vector2(0.0f, 1.0f);
+        blockUVs[9] = new Vector2(0.25f, 1.0f);
 
-        Vector2[] blockUVs = new Vector2[24];
+        // Bottom
+        blockUVs[15] = new Vector2(0.0f, 0.5f);
+        blockUVs[12] = new Vector2(0.25f, 0.5f);
+        blockUVs[14] = new Vector2(0.0f, 0.75f);
+        blockUVs[13] = new Vector2(0.25f, 0.75f);
 
-        blockUVs[0] = new Vector2(umin, vmin);
-        blockUVs[1] = new Vector2(umax, vmin);
-        blockUVs[2] = new Vector2(umin, vmax);
-        blockUVs[3] = new Vector2(umax, vmax);
-        blockUVs[4] = new Vector2(umin, vmax);
-        blockUVs[5] = new Vector2(umax, vmax);
-        blockUVs[6] = new Vector2(umin, vmax);
-        blockUVs[7] = new Vector2(umax, vmax);
-        blockUVs[8] = new Vector2(umin, vmin);
-        blockUVs[9] = new Vector2(umax, vmin);
-        blockUVs[10] = new Vector2(umin, vmin);
-        blockUVs[11] = new Vector2(umax, vmin);
-        blockUVs[12] = new Vector2(umin, vmin);
-        blockUVs[13] = new Vector2(umax, vmax);
-        blockUVs[14] = new Vector2(umin, vmax);
-        blockUVs[15] = new Vector2(umax, vmin);
-        blockUVs[16] = new Vector2(umin, vmin);
-        blockUVs[17] = new Vector2(umin, vmax);
-        blockUVs[18] = new Vector2(umax, vmax);
-        blockUVs[19] = new Vector2(umax, vmin);
-        blockUVs[20] = new Vector2(umin, vmin);
-        blockUVs[21] = new Vector2(umax, vmax);
-        blockUVs[22] = new Vector2(umin, vmax);
-        blockUVs[23] = new Vector2(umax, vmin);
+        // North
+        blockUVs[0] = new Vector2(0.25f, 0.75f);
+        blockUVs[1] = new Vector2(0.5f, 0.75f);
+        blockUVs[2] = new Vector2(0.25f, 1.0f);
+        blockUVs[3] = new Vector2(0.5f, 1.0f);
+
+        // South
+        blockUVs[6] = new Vector2(0.5f, 0.75f);
+        blockUVs[7] = new Vector2(0.75f, 0.75f);
+        blockUVs[10] = new Vector2(0.5f, 1.0f);
+        blockUVs[11] = new Vector2(0.75f, 1.0f);
+
+        // East
+        blockUVs[23] = new Vector2(0.25f, 0.5f);
+        blockUVs[20] = new Vector2(0.5f, 0.5f);
+        blockUVs[22] = new Vector2(0.25f, 0.75f);
+        blockUVs[21] = new Vector2(0.5f, 0.75f);
+
+        // West
+        blockUVs[19] = new Vector2(0.5f, 0.5f);
+        blockUVs[16] = new Vector2(0.75f, 0.5f);
+        blockUVs[18] = new Vector2(0.5f, 0.75f);
+        blockUVs[17] = new Vector2(0.75f, 0.75f);
 
         _mesh.uv = blockUVs;
     }
@@ -78,52 +72,48 @@ public class UVMap : MonoBehaviour, ITransparentRenderer
     {
         Vector2[] blockUVs = (Vector2[])_mesh.uv.Clone();
 
-        float tilePerc = 1 / PixelSize;
-        float umin = tilePerc;
-        float umax = tilePerc * 2;
-        float vmin = tilePerc * y;
-        float vmax = tilePerc * (y + 1);
-        Vector2 minmin = new Vector2(umin, vmin);
-        Vector2 maxmin = new Vector2(umax, vmin);
-        Vector2 minmax = new Vector2(umin, vmax);
-        Vector2 maxmax = new Vector2(umax, vmax);
         switch (face)
         {
             case BlockFace.Top:
-                blockUVs[4] = minmin;
-                blockUVs[5] = maxmin;
-                blockUVs[8] = minmax;
-                blockUVs[9] = maxmax;
+                blockUVs[4] = new Vector2(0.0f, 0.25f);
+                blockUVs[5] = new Vector2(0.25f, 0.25f);
+                blockUVs[8] = new Vector2(0.0f, 0.5f);
+                blockUVs[9] = new Vector2(0.25f, 0.5f);
                 break;
+
             case BlockFace.Bottom:
-                blockUVs[12] = minmax;
-                blockUVs[13] = maxmin;
-                blockUVs[14] = maxmax;
-                blockUVs[15] = minmin;
+                blockUVs[15] = new Vector2(0.0f, 0.0f);
+                blockUVs[12] = new Vector2(0.25f, 0.0f);
+                blockUVs[14] = new Vector2(0.0f, 0.25f);
+                blockUVs[13] = new Vector2(0.25f, 0.25f);
                 break;
+
             case BlockFace.North:
-                blockUVs[0] = minmax;
-                blockUVs[1] = maxmax;
-                blockUVs[2] = minmin;
-                blockUVs[3] = maxmin;
+                blockUVs[0] = new Vector2(0.25f, 0.25f);
+                blockUVs[1] = new Vector2(0.5f, 0.25f);
+                blockUVs[2] = new Vector2(0.25f, 0.5f);
+                blockUVs[3] = new Vector2(0.5f, 0.5f);
                 break;
+
             case BlockFace.South:
-                blockUVs[6] = minmin;
-                blockUVs[7] = maxmin;
-                blockUVs[10] = minmax;
-                blockUVs[11] = maxmax;
+                blockUVs[6] = new Vector2(0.5f, 0.25f);
+                blockUVs[7] = new Vector2(0.75f, 0.25f);
+                blockUVs[10] = new Vector2(0.5f, 0.5f);
+                blockUVs[11] = new Vector2(0.75f, 0.5f);
                 break;
+
             case BlockFace.East:
-                blockUVs[20] = minmax;
-                blockUVs[21] = maxmin;
-                blockUVs[22] = maxmax;
-                blockUVs[23] = minmin;
+                blockUVs[23] = new Vector2(0.25f, 0.0f);
+                blockUVs[20] = new Vector2(0.5f, 0.0f);
+                blockUVs[22] = new Vector2(0.25f, 0.25f);
+                blockUVs[21] = new Vector2(0.5f, 0.25f);
                 break;
+
             case BlockFace.West:
-                blockUVs[16] = minmax;
-                blockUVs[17] = maxmin;
-                blockUVs[18] = maxmax;
-                blockUVs[19] = minmin;
+                blockUVs[19] = new Vector2(0.5f, 0.0f);
+                blockUVs[16] = new Vector2(0.75f, 0.0f);
+                blockUVs[18] = new Vector2(0.5f, 0.25f);
+                blockUVs[17] = new Vector2(0.75f, 0.25f);
                 break;
         }
 
