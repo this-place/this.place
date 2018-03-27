bebb7cb8bd5b305f258791d54b994cd391d59c78
Stanley Tay <saintpatricktay@gmail.com>
2018-03-28 01:11:46 +0800

    feat: signboard

---
+++ Assets/Scripts/SignInfo.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SignInfo : MonoBehaviour
+{
+
+    public GameObject Canvas;
+
+    void Start()
+    {
+        Canvas.SetActive(false);
+    }
+
+    void Update()
+    {
+        if (Canvas.activeSelf)
+        {
+            Canvas.transform.rotation = Camera.main.transform.rotation;
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Canvas.SetActive(true);
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Canvas.SetActive(false);
+        }
+    }
+}
