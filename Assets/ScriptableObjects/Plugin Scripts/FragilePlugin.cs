7658bdb9b633912a8a4132930028889e1d856117
nicholaschuayunzhi <nicholaschuayunzhi@gmail.com>
2018-04-08 14:59:37 +0800

    fix: partcle effects removed on scene restart

--- Assets/ScriptableObjects/Plugin Scripts/FragilePlugin.cs
+++ Assets/ScriptableObjects/Plugin Scripts/FragilePlugin.cs
@@ -16,6 +16,7 @@ public class FragilePlugin : BlockPlugin
         if (!_isDestroyingItself && (_block.IsTranslating() || _block.IsPlayerStandingOn()))
         {
             _fragileEmitterInstantiated = Instantiate(FragileEmitter, _block.gameObject.transform.position, _block.gameObject.transform.rotation) as ParticleSystem;
+            UnityEngine.SceneManagement.SceneManager.MoveGameObjectToScene(_fragileEmitterInstantiated.gameObject, _block.gameObject.scene);
             _fragileEmitterInstantiated.GetComponent<ParticleSystemRenderer>().material = _block.gameObject.GetComponent<Renderer>().material;
             _fragileEmitterInstantiated.Play();
             Destroy(_fragileEmitterInstantiated.gameObject, BreakAfterTime + 1.5f);
