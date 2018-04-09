7d598634e9377f7aa0f93ca1301570d831e107c4
A0162011A <e0112235@u.nus.edu>
2018-04-09 12:09:29 +0800

    chore: update collectibles collider to ontrigger

---
+++ Assets/Scripts/CollectiblesScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectiblesScript : MonoBehaviour {
+    public int optionalNumber = 0;
+
+	void Start ()
+    {
+		
+	}
+	
+	void Update ()
+    {
+		
+	}
+
+    private void OnTriggerEnter(Collision collision)
+    {
+        Destroy(this.gameObject);
+    }
+}
