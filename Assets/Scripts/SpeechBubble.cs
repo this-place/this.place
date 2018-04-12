2be88e36402d338aad3313b24db923aa9f981f3f
Nicholas Chua <nicholaschuayunzhi@gmail.com>
2018-04-12 14:53:01 +0800

    feat: add speech bubbles (#157)
    * feat: add speech bubbles
    
    * chore: expose variables and clean up formatting

---
+++ Assets/Scripts/SpeechBubble.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SpeechBubble : MonoBehaviour
+{
+
+    public GameObject SpeechCanvasRoot;
+    public float FadeOutTime = 5;
+
+    private GameObject _player;
+
+    void Start()
+    {
+        SpeechCanvasRoot.SetActive(false);
+    }
+
+    void Update()
+    {
+        if (SpeechCanvasRoot.activeSelf)
+        {
+            SpeechCanvasRoot.transform.forward = Camera.main.transform.forward;
+            SpeechCanvasRoot.transform.position = _player.transform.position;
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            SpeechCanvasRoot.SetActive(true);
+            _player = other.gameObject;
+            StartCoroutine(WaitThenDestroy());
+        }
+    }
+
+    IEnumerator WaitThenDestroy()
+    {
+        yield return new WaitForSeconds(FadeOutTime);
+        Destroy(gameObject);
+        Destroy(SpeechCanvasRoot);
+    }
+}
